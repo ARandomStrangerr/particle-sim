bdf62d0af899a62c14ed185794bcfd7926c86ddf
thanhdo <billslim0996@gmail.com>
2024-08-28 20:51:36 -0700

    illustrations

--- main.cpp
+++ main.cpp
@@ -24,20 +24,28 @@ void constantVelocityObject() {
 	float dt = 0.06;
 
 	// if we move A units buy 1 sec, then we move A*dt units by dt sec
-	CircleObject object (10, 10, 30, sf::Color(205,180,219), 10*dt, 10*dt);
+	CircleObject object (10, 10, 30, sf::Color(205,180,219), 50*dt, 50*dt);
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Draw An Object with Constant Velocity");
 
+	bool startSimulation = false;
+
 	while (window.isOpen()) {
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed)
 				window.close();
-		window.clear();
-		window.draw(object.getShape());
-		object.move(dt); // calculate the object position after dt second
-		window.display();
-		std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(dt * 1000))); //sleep for that much time before updating
+			if (event.type == sf::Event::KeyPressed)
+				if (event.key.code == sf::Keyboard::Space)
+					startSimulation = !startSimulation;
+		}
+		if (startSimulation) {
+			window.clear();
+			window.draw(object.getShape());
+			object.move(dt); // calculate the object position after dt second
+			window.display();
+			std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(dt * 1000))); //sleep for that much time before updating
+		}
 	}
 }
 
@@ -72,6 +80,6 @@ void freeFallObject() {
 }
 
 int main(){
-	freeFallObject();
+	constantVelocityObject();
 	return 0;
 }
