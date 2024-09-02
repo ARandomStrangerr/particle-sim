2a795e72f2f40962e389371dae056efd310c6127
thanhdo <billslim0996@gmail.com>
2024-09-02 00:46:19 -0700

    add two objects into the screen

--- main.cpp
+++ main.cpp
@@ -80,7 +80,43 @@ void freeFallObject() {
 	}
 }
 
+void twoObjects() {
+	float dt = 0.01;
+	CircleObject object1(200, 30, 10, sf::Color(205,180,219), 50*dt, 50*dt),
+		object2(400, 30, 10, sf::Color(205,180,219), -30*dt, 50*dt);
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Draw A Free Fall Object");
+
+	bool startSimulation = false;
+
+	while(window.isOpen()) {
+		sf::Event event;
+		if (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+			else if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Space)
+					startSimulation = !startSimulation;
+			}
+		}
+
+		if (startSimulation) {
+			window.clear();
+			window.draw(object1.getShape());
+			window.draw(object2.getShape());
+			object1.accelerate(0, 981);
+			object2.accelerate(0, 981);
+			object1.stayInsideScreen(800, 600);
+			object2.stayInsideScreen(800, 600);
+			object1.move(dt);
+			object2.move(dt);
+			window.display();
+			std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(dt * 1000)));
+		}
+	}
+}
+
 int main(){
-	freeFallObject();
+	twoObjects();
 	return 0;
 }
